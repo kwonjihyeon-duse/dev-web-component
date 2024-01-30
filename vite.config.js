2fa49624b9234f28b2d4cd5ac525aae8d06ae91d
adanbae-dev <mostbae.dev@gmail.com>
2024-01-30 15:17:09 +0900

    setting: storybook-addon-coverage

--- vite.config.js
+++ vite.config.js
@@ -1,59 +1,72 @@
 import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import svg from 'vite-plugin-svgo';
+import istanbul from 'vite-plugin-istanbul';
 
-export default defineConfig({
-  assetsInclude: ['/sb-preview/**'],
-  publicDir: false,
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
     },
-  },
-  build: {
-    lib: {
-      fileName: 'index',
-      entry: 'src/index.ts',
-      format: ['es'],
-      // fileName: (format) => `index.${format}.js`,
-      name: 'DuseWebComponent',
-    },
-    minify: false,
-    rollupOptions: {
-      // external: /^lit/
-      output: {
-        assetFileNames: (assetInfo) => {
-          let extType = assetInfo.name.split('.').at(1);
-          console.log('extType', extType);
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-            extType = 'img';
-          }
-          return `assets/${extType}/[name]-[hash][extname]`;
-        },
-        chunkFileNames: (assetInfo) => {
-          let extType = assetInfo.name.split('.').at(1);
-          console.log('assetInfo', assetInfo);
-          return 'assets/js/[name]-[hash].js';
-        },
-        // entryFileNames: 'assets/js/[name]-[hash].js',
+    assetsInclude: ['/sb-preview/**'],
+    publicDir: false,
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src'),
       },
     },
-  },
-  plugins: [
-    svg({
-      multipass: true,
-      plugins: [
-        {
-          name: 'preset-default',
-          params: {
-            overrides: {
-              convertColors: {
-                currentColor: true,
+    build: {
+      lib: {
+        fileName: 'index',
+        entry: 'src/index.ts',
+        format: ['es'],
+        // fileName: (format) => `index.${format}.js`,
+        name: 'DuseWebComponent',
+      },
+      minify: false,
+      rollupOptions: {
+        // external: /^lit/
+        output: {
+          assetFileNames: (assetInfo) => {
+            let extType = assetInfo.name.split('.').at(1);
+            console.log('extType', extType);
+            if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+              extType = 'img';
+            }
+            return `assets/${extType}/[name]-[hash][extname]`;
+          },
+          chunkFileNames: (assetInfo) => {
+            let extType = assetInfo.name.split('.').at(1);
+            console.log('assetInfo', assetInfo);
+            return 'assets/js/[name]-[hash].js';
+          },
+          // entryFileNames: 'assets/js/[name]-[hash].js',
+        },
+      },
+    },
+    plugins: [
+      svg({
+        multipass: true,
+        plugins: [
+          {
+            name: 'preset-default',
+            params: {
+              overrides: {
+                convertColors: {
+                  currentColor: true,
+                },
               },
             },
           },
-        },
-      ],
-    }),
-  ],
+        ],
+      }),
+      istanbul({
+        include: 'src/*',
+        exclude: ['node_modules'],
+        extension: ['.js', '.ts'],
+        requireEnv: true,
+      }),
+    ],
+  };
 });
